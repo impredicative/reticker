95d328aa7da8e3e56a946e98576261468c2793c5
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2021-03-07 11:55:26 -0500

    Add setup module

---
+++ setup.py
@@ -0,0 +1,32 @@
+"""Package installation setup."""
+import os
+import re
+from pathlib import Path
+from typing import Match, cast
+
+from setuptools import find_packages, setup
+
+_DIR = Path(__file__).parent
+
+
+setup(
+    name="reticker",
+    author="Ouroboros Chrysopoeia",
+    author_email="impredicative@users.noreply.github.com",
+    version=cast(Match, re.fullmatch(r"refs/tags/v?(?P<ver>\S+)", os.environ["GITHUB_REF"]))["ver"],  # Ex: GITHUB_REF="refs/tags/1.2.3"; version="1.2.3"
+    description="Use a regular expression to extract US style stock tickers from text",
+    keywords="regex regexp stock text ticker",
+    long_description=(_DIR / "README.md").read_text().strip(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/impredicative/reticker/",
+    packages=find_packages(exclude=["scripts"]),
+    python_requires=">=3.8",
+    classifiers=[  # https://pypi.org/classifiers/
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Topic :: Office/Business :: Financial :: Investment",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Filters",
+    ],
+)
