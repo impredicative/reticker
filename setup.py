a26f31a9b40130401ba8fe5491dbe76a7670eb03
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2021-03-07 19:51:45 -0500

    Include txt file in package data

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ from typing import Match, cast
 from setuptools import find_packages, setup
 
 _DIR = Path(__file__).parent
-
+_PACKAGE_NAME = "reticker"
 
 setup(
-    name="reticker",
+    name=_PACKAGE_NAME,
     author="Ouroboros Chrysopoeia",
     author_email="impredicative@users.noreply.github.com",
     version=cast(Match, re.fullmatch(r"refs/tags/v?(?P<ver>\S+)", os.environ["GITHUB_REF"]))["ver"],  # Ex: GITHUB_REF="refs/tags/1.2.3"; version="1.2.3"
@@ -20,6 +20,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/impredicative/reticker/",
     packages=find_packages(exclude=["scripts"]),
+    package_data={_PACKAGE_NAME: ["config/blacklist.txt"]},
     python_requires=">=3.8",
     classifiers=[  # https://pypi.org/classifiers/
         "Programming Language :: Python :: 3.8",
