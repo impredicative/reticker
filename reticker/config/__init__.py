03bd652445f4c6c0f29f143f2a8765a1589116df
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2021-03-07 23:00:14 -0500

    Support Python 3.8 style type-annotation

--- reticker/config/__init__.py
+++ reticker/config/__init__.py
@@ -1,7 +1,7 @@
 """Package config."""
 from pathlib import Path
-from typing import Final, Set
+from typing import Final, List, Set
 
 _CONFIG_PATH: Final[Path] = Path(__file__).parent
-BLACKLIST_PATHS: Final[list[Path]] = list((_CONFIG_PATH / "blacklist").glob("*.txt"))
+BLACKLIST_PATHS: Final[List[Path]] = list((_CONFIG_PATH / "blacklist").glob("*.txt"))
 BLACKLIST: Set[str] = set(term for path in BLACKLIST_PATHS for term in path.read_text().strip().split("\n"))
