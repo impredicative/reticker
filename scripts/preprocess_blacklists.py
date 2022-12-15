a5710bcfe8fcc3fa8aadf7faa9718ee6978a6dad
worker701 <120233263+worker701@users.noreply.github.com>
2022-12-15 04:00:51 +0000

    Address false pylint issue in script

--- scripts/preprocess_blacklists.py
+++ scripts/preprocess_blacklists.py
@@ -3,6 +3,6 @@ from reticker.config import BLACKLIST_PATHS
 
 for path in BLACKLIST_PATHS:
     blacklist = path.read_text().strip().split("\n")
-    blacklist = [stripped_term.upper() for term in blacklist if (stripped_term := term.strip())]
+    blacklist = [stripped_term.upper() for term in blacklist if (stripped_term := term.strip())]  # pylint: disable=used-before-assignment
     blacklist = sorted(set(blacklist))
     path.write_text("\n".join(blacklist))
